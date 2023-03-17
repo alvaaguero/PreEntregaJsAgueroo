26ff70d11526d9432b532ae97e71d0abd10e1f98
alvaaguero <agueroalvaro010@gmail.com>
2023-03-16 23:53:24 -0300

    ss

--- main.js
+++ main.js
@@ -66,3 +66,66 @@ else{
     productosRecomendados(12)
 }
 
+//lista de producs
+class Producto{
+    constructor(nombreProdu, precioProdu, diaProdu){
+        this.nombreProdu = nombreProdu;
+        this.precioProdu = precioProdu;
+        this.diaProdu = diaProdu;
+    }
+
+    get_datos(){
+        console.log("............")
+        console.log("Nombre del producto:", this.nombreProdu);
+        console.log("Precio del producto:", this.precioProdu);
+        console.log("Dia del producto", this.diaProdu);
+        console.log("............")
+    }
+}
+
+let lista_produ = []
+
+lista_produ.push( new Producto("Crecimiento", 300 , "Martes"));
+lista_produ.push( new Producto("Mantenimiento", 650 , "Jueves"));
+lista_produ.push( new Producto("Disminucion", 330 , "viernes" ));
+
+
+
+console.log(lista_produ);
+
+for ( let producto of lista_produ){
+    producto.get_datos();
+}
+//Seleccion de produ
+
+function busqueda_producto(){
+    return servicio.nombreProdu == seleccion_produ
+}
+
+let seleccion_produ = prompt("Ingrese producto\nCrecimiento\nMantenimiento\nDisminucion");
+
+let resultado_busqueda = lista_produ.find( busqueda_producto );
+
+while(resultado_busqueda == undefined){
+
+        alert("No se encontro el producto")
+        seleccion_produ = prompt("Ingrese nuevamente el producto (respete las  mayusculas y minusculas)\nCrecimiento\nMantenimiento\nDisminucion")
+        resultado_busqueda = lista_produ.find( busqueda_producto )
+}
+
+//Producto seleccionado
+
+let precio_producto = 0
+
+if ( seleccion_produ == "Crecimiento"){
+    precio_producto = crece
+}
+else if ( seleccion_produ == "Mantenimiento"){
+    precio_servicio = mantener
+}
+else if ( seleccion_produ == "Disminucion"){
+    precio_producto = disminuir
+}
+else{
+    console.log("Producto erroneo")
+}
