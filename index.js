eee096a31ea19a4a1b3aa6d0408692a360d04378
alvaaguero <agueroalvaro010@gmail.com>
2023-05-11 00:16:39 -0300

    Productos

---
+++ index.js
@@ -0,0 +1,19 @@
+function rederProductos() {
+    const productos = cargarProductosLS();
+    let salida = "";
+
+    for (producto of productos){
+        salida += `<div class="col-md-3 my-3">
+            <div class="card text-center border-0">
+                <img src="${"img/" + producto.imagen}" alt="${producto.nombre}" class="card-img-top" />
+                <div class="card-body">
+                    <p class="card-text">${producto.nombre}</p>
+                    <p><button class="btn btn-warning">Agregar (+)</button>
+                </div>
+            </div>
+        </div>`;
+    }
+
+    document.getElementById("productos").innerHTML = salida;
+}
+rederProductos(); 
